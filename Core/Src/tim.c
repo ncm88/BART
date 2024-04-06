2bcd413d951c02ac376861e6a7858ea7a586e10b
Nolan <62817066+ncm88@users.noreply.github.com>
2024-04-06 15:25:08 -0700

    byte-by-byte streaming changes

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -166,9 +166,9 @@ void MX_TIM7_Init(void)
 
   /* USER CODE END TIM7_Init 1 */
   htim7.Instance = TIM7;
-  htim7.Init.Prescaler = 9;
+  htim7.Init.Prescaler = 0;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 7999;
+  htim7.Init.Period = 799;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
   {
